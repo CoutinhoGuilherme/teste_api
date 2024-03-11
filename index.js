72daa0e3944a190c9641617496a90f953631fb27
Guilherme Coutinho de Moura <69174628+CoutinhoGuilherme@users.noreply.github.com>
2024-03-11 23:30:49 +0000

    Continuacao aula testes

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const port = 3000;
 // Configuração do MySQL
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: 'root', // Seu nome de usuário do MySQL
-  password: '123456', // Sua senha do MySQL
-  database: 'usersdb'
+  user: process.env.MYSQL_USER, // Usa variáveis de ambiente
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 // Conectar ao banco de dados
@@ -30,6 +30,7 @@ app.post('/users', (req, res) => {
   const INSERT_USER_QUERY = `INSERT INTO users (name, email) VALUES (?, ?)`;
   connection.query(INSERT_USER_QUERY, [name, email], (err, results) => {
     if (err) throw err;
+    res.statusCode=201;
     res.send('Usuário criado com sucesso');
   });
 });
@@ -58,3 +59,5 @@ app.get('/users/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
+
+module.exports = app;
