358bd8104bbd720150a8b9b53849c10308cc073d
Guilherme Coutinho de Moura <69174628+CoutinhoGuilherme@users.noreply.github.com>
2024-03-12 00:51:35 +0000

    Teste API continuacao

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 // Configuração do MySQL
 const connection = mysql.createConnection({
   host: 'localhost',
-  user: process.env.MYSQL_USER, // Usa variáveis de ambiente
+  user: process.env.MYSQL_USER, 
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
 });
