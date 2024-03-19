43b99b2077ca6fa0f49e3d1d6a8533a45c139262
Guilherme Coutinho de Moura <69174628+CoutinhoGuilherme@users.noreply.github.com>
2024-03-19 01:06:11 +0000

    Teste

--- api.test.js
+++ api.test.js
@@ -1,6 +1,8 @@
 const supertest = require('supertest');
 const {app, server, connection} = require('./index.js'); // Importe seu aplicativo Express
 
+let idCriado;
+
 describe('Teste GET /users', () => {
   it('deve responder com status 200', async () => {
     const response = await supertest(app).get('/users');
@@ -18,10 +20,24 @@ describe('Teste Post /users', () => {
   it('deve responder com status 201', async () => {
     const response = await supertest(app).post('/users', {name: "Fulano de Tal", email: "fulano@gmail.com"});
     expect(response.statusCode).toBe(201);
-
+    idCriado = response.body.id;
     expect(response.body).toHaveProperty("id");    
     expect(response.body.id).toBeGreaterThan(0);    
     console.log("O id criado foi: " + response.id)
+  });
+});
+
+describe('Atualiza usuario Put /users', () => {
+  it('deve responder com status 204', async () => {
+    const response = await supertest(app).put('/users/1', {name: "Fulano de Tal", email: "fulano@gmail.com"});
+    expect(response.statusCode).toBe(204);
+  });
+});
+
+describe('Deleta usuario /users', () => {
+  it('deve responder com status 204', async () => {
+    const response = await supertest(app).delete('/users/1' + idCriado);
+    expect(response.statusCode).toBe(204);
   });
 });
 
